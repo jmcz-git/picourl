9119da2f2424715f211a45aaebae69a3a34eacf5
juancho.villa@icloud.com <juancho.villa@icloud.com>
2020-12-20 20:02:33 -0700

    Implement basic testing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#[allow(dead_code)]
+fn shorter(s: &str) -> &str {
+  s
 }
+
+fn main() {
+  println!("{}", shorter("http://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR"));
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn it_works() {
+        assert_eq!(
+          shorter("http://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR"),
+          "https://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR"
+        );
+    }
+}
+
