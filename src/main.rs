9158d9f42f5a30c632369e0c0a57e43899d422c9
David A. Viramontes <dviramontes@gmail.com>
2020-12-20 20:24:16 -0700

    format and name test

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-#[allow(dead_code)]
 fn shorter(_url: &str) -> String {
-  let s = String::from("http://tinyurl.com/jlg8zpc");
-  s
+    String::from("http://tinyurl.com/jlg8zpc")
 }
 
 fn main() {
-  println!("{}", shorter("http://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR"));
+    println!(
+        "{}",
+        shorter("http://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR")
+    );
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
-    fn it_works() {
-        let s = shorter("http://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR");
+    fn it_returns_a_shorter_url() {
+        let s = shorter(
+            "http://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR",
+        );
         assert_eq!(s.len(), 26);
     }
 }
-
